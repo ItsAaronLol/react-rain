9301131cb7362c099e28adf4a2ff6ad02a7fd62f
Aaron Wong <aaronywwong@gmail.com>
2016-08-23 21:14:58 +0930

    Add ReactApp component

---
+++ app/components/RainApp.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var RainApp = React.createClass({
+  render: function () {
+    return (
+      <div>
+        RainApp.jsx
+      </div>
+    )
+  }
+});
+
+module.exports = RainApp;
